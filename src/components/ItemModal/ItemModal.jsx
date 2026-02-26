2db488a16b24110b5e2612a392a9d67922c95c70
Wahid Fayeq <wahid.fayaq@yahoo.com>
2026-02-26 18:48:43 -0500

    New update for my project

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -7,8 +7,26 @@ function ItemModal({ activeModal, card, onClose }) {
   return (
     <div className={`modal ${isOpen ? "modal_opened" : ""}`}>
       <div className="modal__preview">
-        <button type="button" className="modal__close" onClick={onClose}>
-          ×
+        <button
+          type="button"
+          className="modal__close"
+          onClick={onClose}
+          aria-label="Close"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="modal__close-icon"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
         </button>
 
         <img src={card.link} alt={card.name} className="modal__preview-image" />
