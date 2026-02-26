4298f95a9c6e2fef575e61ae4e252390849e65a4
Wahid Fayeq <wahid.fayaq@yahoo.com>
2026-02-26 12:45:56 -0500

    New update for my project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,48 +19,62 @@ function Header({ weatherData, handleAddClick }) {
 
   return (
     <header className="header">
-      <div className="header__left">
-        <img src={logo} alt="WTWR logo" className="header__logo" />
-        <p className="header__date-location">
-          {currentDate}, {city}
-        </p>
-      </div>
+      <div className="header__content">
+        <div className="header__left">
+          <img src={logo} alt="WTWR logo" className="header__logo" />
+          <p className="header__date-location">
+            {currentDate}, {city}
+          </p>
+        </div>
 
-      <nav
-        className={`header__nav ${
-          isMobileMenuOpened ? "header__nav_mobile-open" : ""
-        }`}
-      >
-        {isMobileMenuOpened && (
+        <nav
+          className={`header__nav ${
+            isMobileMenuOpened ? "header__nav_mobile-open" : ""
+          }`}
+        >
+          {isMobileMenuOpened && (
+            <button
+              type="button"
+              className="header__nav-close"
+              onClick={toggleMobileMenu}
+              aria-label="Close"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="header__nav-close-icon"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          )}
+
           <button
             type="button"
-            className="header__nav-close"
-            onClick={toggleMobileMenu}
-            aria-label="Close menu"
+            className="header__add-button"
+            onClick={handleAddClick}
           >
-            X
+            + Add clothes
           </button>
-        )}
-        <button
-          type="button"
-          className="header__add-button"
-          onClick={handleAddClick}
-        >
-          + Add clothes
-        </button>
 
-        <div className="header__user">
-          <p className="header__username">Terrence Tegegne</p>
+          <div className="header__user">
+            <p className="header__username">Terrence Tegegne</p>
 
-          <img
-            src={avatar}
-            alt="User avatar"
-            className="header__avatar-image"
-          />
-        </div>
-      </nav>
-
-      {/* MOBILE MENU TOGGLE BUTTON */}
+            <img
+              src={avatar}
+              alt="User avatar"
+              className="header__avatar-image"
+            />
+          </div>
+        </nav>
+      </div>
       <button
         type="button"
         className="header__menu-button"
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,9 +1,7 @@
 import "./ModalWithForm.css";
 function ModalWithForm({ children, buttonText, title, isOpen, onClose }) {
   return (
-    <div
-      className={`modal ${isOpen ? 'modal_opened' : '' }`}> 
-    
+    <div className={`modal ${isOpen ? "modal_opened" : ""}`}>
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
         <button
@@ -16,13 +14,13 @@ function ModalWithForm({ children, buttonText, title, isOpen, onClose }) {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
-            class="size-6"
+            className="modal__close-icon"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M6 18 18 6M6 6l12 12"
             />
           </svg>
